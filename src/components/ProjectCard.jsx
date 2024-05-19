d43f57461a904f823fd186d3efdc515e359493a6
Purabi22 <119959933+Purabi22@users.noreply.github.com>
2024-05-19 16:05:44 +0530

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import "./ProjectCard.css"
-const ProjectCard = ({details}) => {
-  return (
-<div className='project-card'>
-    <h6>{details.title}</h6>
-    <ul>
-        {details.description.map((item) =>(
-            <li key={item}>{item}</li>
-        ))}
-    </ul>
-    </div>
-  )
-}
+import React from 'react';
+import "./ProjectCard.css";
 
-export default ProjectCard
+const ProjectCard = ({ details }) => {
+    return (
+        <div className='project-card'>
+            <h6>
+                <a href={details.url} target="_blank" rel="noopener noreferrer">
+                    {details.title}
+                </a>
+            </h6>
+            <ul>
+                {details.description.map((item) => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ProjectCard;
